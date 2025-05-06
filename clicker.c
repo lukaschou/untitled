4fe2f7017899885011cd69f8e21ec1768360c7b0
Lukas Chou <lukaschou21@icloud.com>
2025-05-05 23:50:12 -0700

    More setup and comments

--- clicker.c
+++ clicker.c
@@ -4,22 +4,29 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-struct termios og_termios;
+struct termios og_termios; // User's original terminal config
 
+/* Clears screen, perrors with a given string and exits the program */
 void die(char *s) {
     perror(s);
     exit(EXIT_FAILURE);
 }
 
+/* Reset terminal flags to original config */
 void disable_raw_mode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios) == -1)
         die("tcsetattr");
 }
 
+/* Saves user's original terminal config and converts to raw mode */
 void enable_raw_mode() {
     if (tcgetattr(STDIN_FILENO, &og_termios) == -1) 
         die("tcgetattr");
-    atexit(disable_raw_mode);
+    if (atexit(disable_raw_mode) != 0) {
+         /* Manually exit because atexit does not set errno on failure */
+        fprintf(stderr, "atexit: cannot set exit function\n");
+        exit(EXIT_FAILURE);
+    }
 
     struct termios raw = og_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -33,18 +40,46 @@ void enable_raw_mode() {
         die("tcsetattr");
 }
 
+/*
+ * Read bytes one at a time from standard input
+ * Will need to handle multi-byte keypresses like arrowkeys
+ */
+int readKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1) die("read");
+    }
+    return c;
+}
+
+void processKeyPress() {
+    char c = readKey();
+
+    switch (c) {
+        case 'q':
+            exit(EXIT_SUCCESS);
+            break;
+        default:
+            printf("CHAR: %d\r\n", c);
+    }
+}
+
+/* 
+ * Clear screen and reposition cursor to top-left corner.
+ * We should make sure to call this whenever we exit too (die, quit, etc...)
+ * Also error handling
+ */
+void refreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 int main() {
     enable_raw_mode();
-    char c;
+    refreshScreen();
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1) die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q') break;
+        processKeyPress();
     }
     return EXIT_SUCCESS;
 }
