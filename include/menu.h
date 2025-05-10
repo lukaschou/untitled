24097fcd5fa6215552357aa72088734908bda50f
calebchou21 <calebchou21@gmail.com>
2025-05-10 12:52:39 -0600

    Menus
    Co-authored-by: Lukas Chou <lukaschou@users.noreply.github.com>

--- include/menu.h
+++ include/menu.h
@@ -1,22 +1,15 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include "input.h"
-
 // This needs to be in the order they are printed
 typedef enum {
     NEWGAME = 1
 } MenuOption;
 
 void draw_menu(void);
-long get_menu_option();
+long get_menu_opt(long max_opts);
 void run_menu(void);
-void handle_invalid_opt(char *val);
-int execute_opt(long opt);
+void handle_invalid_opt();
+void execute_opt(long opt);
 
 #endif
--- src/menu.c
+++ src/menu.c
@@ -1,36 +1,45 @@
 #include "menu.h"
 #include "input.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /* Prints introduction text and lists menu options */
 void draw_menu(void) {
-    printf("Welcome to Space Freight\n\n");
+    printf("Welcome to \033[1mSpace Freight\033[22m\n\n");
     printf("1. New Game\n");
+    printf("2. Continue\n");
+    printf("3. Settings\n\n");
     if (fflush(stdout)) {
         perror("fflush");
         exit(EXIT_FAILURE);
     }
 }
 
-/* 
+/*
  * Gets input from a user until ONLY a valid menu option number is input.
  * Returns a long because strtol does, and who cares. If an invalid
  * value is received, clears the screen and displays the menu and prompt.
  */
-long get_menu_opt() {
+long get_menu_opt(long max_opts) {
     char input[MAX_INPUT_SIZE];
     long value = 0;
-    int success = 1;
+    int success;
     do {
+        success = 1;
         char *endptr = NULL;
         printf("Enter option: ");
+
         read_input(input);
         input[strcspn(input, "\n")] = '\0'; // strip newline
         errno = 0;
         value = strtol(input, &endptr, 10);
-        if (errno || input[0] == '\0' || endptr[0] != '\0') {
+        if (errno || input[0] == '\0' || endptr[0] != '\0'
+            || value < 0 || value > max_opts) {
             success = 0;
-            handle_invalid_opt(input);
+            handle_invalid_opt();
         }
         if (fflush(stdout)) {
             perror("fflush");
@@ -41,36 +50,41 @@ long get_menu_opt() {
     return value;
 }
 
-void handle_invalid_opt(char *val) {
-    printf("Invalid option: %s\n", val); 
-    printf("Press any key to continue\n");
+void handle_invalid_opt() {
+    printf("Invalid option\n");
+    printf("Press Enter to continue\n");
     if (fflush(stdout)) {
         perror("fflush");
         exit(EXIT_FAILURE);
     }
-    fgetc(stdin);
+
+    char c;
+    do {
+        c = fgetc(stdin);
+        if (c == EOF) {
+            if (feof(stdin)) {
+                clearerr(stdin);
+                c = 'p';
+            } else {
+                perror("fgetc");
+                exit(EXIT_FAILURE);
+            }
+        }
+    } while (c != '\n');
     clear_screen();
     draw_menu();
 }
 
-int execute_opt(long opt) {
-   switch (opt) {
-       case NEWGAME:
-           printf("New game selected...\n");
-           return 0;
-        default: {
-           char opt_str[30]; // Enough to hold a long string
-           snprintf(opt_str, 30, "%ld", opt);
-           handle_invalid_opt(opt_str);
-           return -1;
-        }
-    } 
+/* Executes a menu option. Option MUST be valid */
+void execute_opt(long opt) {
+    switch (opt) {
+        case NEWGAME:
+            printf("New game selected...\n");
+            break;
+    }
 }
 
 void run_menu(void) {
     draw_menu();
-    int err_status;
-    do {
-        err_status = execute_opt(get_menu_opt());
-    } while (err_status);
+    execute_opt(get_menu_opt(1));
 }
